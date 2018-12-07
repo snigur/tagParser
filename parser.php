550f41987b292d88de1bf5121639925e6209873d
Dima <d.kolotugin@ukr.net>
2018-12-07 15:34:22 +0200

    compl act

---
+++ parser.php
@@ -0,0 +1,16 @@
+<?php
+
+
+require_once 'pr.php';
+
+class Parser implements ParserInterface
+{
+	public function process($url,$tag)
+	{
+		return [
+			'fdfd',
+			'fdfdf',
+			'dfdsas'		
+		];
+	}
+}
